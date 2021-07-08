df097ecef27072ebe0ead263f0c20413849ef1a0
IBRA <ibrahimmerhi@mail.ru>
2021-07-08 22:46:02 +0800

    annotation Route && Wildcard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,11 +5,26 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController
 {
+    // composer require annotantion liobary to add route before any function
+    /**
+     *@Route("/hello")
+     */
     public function HomePage(){
        return new Response('hello this is the first route');
     }
+
+    /**
+     * @Route("/newMessage/{slug}")
+     */
+    public function newMessage($slug){
+        return new Response(sprintf(
+            'this is the second Route "$%s"!'
+            ,$slug));
+    }
+
 
 }
