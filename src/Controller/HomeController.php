bfbd1080bf9979d20336e704a6e1e3dc15b3eaaf
IBRA <ibrahimmerhi@mail.ru>
2021-07-09 12:34:28 +0800

    twig and layouts

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,11 +4,13 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
+   // extends the controller from AbstractController to help us get shortcut for ailas
     // composer require annotantion liobary to add route before any function
     /**
      *@Route("/hello")
@@ -24,6 +26,22 @@ class HomeController
         return new Response(sprintf(
             'this is the second Route "$%s"!'
             ,$slug));
+    }
+
+
+    /**
+     * @Route("/question/{slug}")
+     */
+    public function show($slug){
+      $ansers =[
+          'this is the first answer',
+          'this is the Second answer',
+          'this is the third answer'
+      ];
+        return $this->render('questions/show.html.twig',[
+           'question' => ucwords(str_replace('-',' ',$slug)),
+            'answers' => $ansers
+        ]);
     }
 
 
