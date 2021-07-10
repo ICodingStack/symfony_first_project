d3e96694f98e47d07ce97c70ab5a19a5e5e54265
IBRA <ibrahimmerhi@mail.ru>
2021-07-10 15:21:44 +0800

    s

---
+++ src/Controller/commentsController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class commentsController extends AbstractController
+{
+    /**
+     * @Route("/comment/{id}/vote/{direction}")
+     */
+    public function commentVote($id,$direction){
+
+       if($direction ==="up"){
+         $currentCommentVote =rand(7,100);
+       }else{
+           $currentCommentVote =rand(0,5);
+       }
+        return $this->json([
+            'vote'=>$currentCommentVote
+        ]);
+        #return new JsonResponse(['vote'=>$currentCommentVote]);
+   }
+}
